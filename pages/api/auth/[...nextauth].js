3896d4b79628af152d672f8c1b540f7c7349391c
Gwanggyo Oh <gwanggyooh@Gwanggyoui-MacBookPro.local>
2022-11-12 18:28:16 -0800

    changed

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -16,6 +16,7 @@ export const authOptions = {
             clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
     ],
+    secret: process.env.SECRET,
 
 }
 export default NextAuth(authOptions)
